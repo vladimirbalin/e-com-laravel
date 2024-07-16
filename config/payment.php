84cbdd008797d1363df43070378af094c8754206
vladimirb <vladimirzq@gmail.com>
2024-07-16 17:10:01 +0700

    Yookassa shop id convert type

--- config/payment.php
+++ config/payment.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 return [
     'providers' => [
         'unitpay' => [
-            'public_key' => env('UNITPAY_PUBLIC_KEY',''),
-            'secret_key' => env('UNITPAY_SECRET_KEY',''),
+            'public_key' => env('UNITPAY_PUBLIC_KEY', ''),
+            'secret_key' => env('UNITPAY_SECRET_KEY', ''),
         ],
         'yookassa' => [
-            'login' => env('YOOKASSA_SHOP_ID', ''),
+            'login' => (int) env('YOOKASSA_SHOP_ID', ''),
             'password' => env('YOOKASSA_SECRET_KEY', ''),
         ]
     ]
